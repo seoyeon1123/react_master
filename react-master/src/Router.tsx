213c6a462a051d53790ceb3f327715c6c194e59f
seoyeon1123 <lsy_0906@naver.com>
2024-06-01 18:22:15 +0900

    feat : homePage

--- react-master/src/Router.tsx
+++ react-master/src/Router.tsx
@@ -1,21 +1,19 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Coin from './routes/Coin';
 import Coins from './routes/Coins';
 
-const Router = () => {
+function Router() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/">
-          <Coins />
-        </Route>
         <Route path="/:coinId">
           <Coin />
+        </Route>
+        <Route path="/">
+          <Coins />
         </Route>
       </Switch>
     </BrowserRouter>
   );
-};
-
+}
 export default Router;
--- react-master/src/routes/Coin.tsx
+++ react-master/src/routes/Coin.tsx
@@ -1,12 +1,11 @@
 import { useParams } from 'react-router';
-import React from 'react';
 
 interface RouteParams {
   coinId: string;
 }
 
-const Coin = () => {
+function Coin() {
   const { coinId } = useParams<RouteParams>();
   return <h1>Coin: {coinId}</h1>;
-};
+}
 export default Coin;
